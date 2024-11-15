df9a6aaa90ff94a824eb9bdba781edcf5f5473a0
AdrianALloyd <AdrianALloyd@outlook.com>
2024-11-15 10:12:47 +0000

    Created sql database and data

---
+++ Game_list data.sql
@@ -0,0 +1,45 @@
+USE Game_List;
+
+INSERT INTO Platform(Platform_name)
+VALUES("PC");
+INSERT INTO Platform(Platform_name)
+VALUES("XBOX");
+INSERT INTO Platform(Platform_name)
+VALUES("Playstation");
+INSERT INTO Platform(Platform_name)
+VALUES("PC, XBOX");
+INSERT INTO Platform(Platform_name)
+VALUES("PC, Playstation");
+INSERT INTO Platform(Platform_name)
+VALUES("PC, XBOX, Playstation");
+INSERT INTO Platform(Platform_name)
+VALUES("XBOX, Playstation");
+
+INSERT INTO Game(Game_Name, Genre, Released)
+VALUES("Skyrim", "RPG", "2011-11-11");
+INSERT INTO Developer(Name, Location)
+VALUES("Bethesda Softworks","Maryland");
+
+INSERT INTO Game(Game_Name, Genre, Released)
+VALUES("Call of Duty: Black Ops 6", "FPS", "2024-10-25");
+INSERT INTO Developer(Name, Location)
+VALUES("Treyarch","California");
+
+INSERT INTO Game(Game_Name, Genre, Released)
+VALUES("Minecraft", "Sandbox", "2011-11-18");
+INSERT INTO Developer(Name, Location)
+VALUES("Mojang","Stockholm");
+
+INSERT INTO Game(Game_Name, Genre, Released)
+VALUES("Gears of War", "Third person shooter", "2006-11-07");
+INSERT INTO Developer(Name, Location)
+VALUES("Epic Games","North Carolina");
+
+INSERT INTO Game(Game_Name, Genre, Released)
+VALUES("Grand Theft Auto V", "Action-Adventure", "2013-09-17");
+INSERT INTO Developer(Name, Location)
+VALUES("Rockstar","Edinburgh");
+
+
+
+
